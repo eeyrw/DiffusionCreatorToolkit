f990dafbc1b5a5ddcf36411de58ad2728afb4659
Your Name <you@example.com>
2022-12-05 16:07:53 +0800

    M

--- Creator.py
+++ Creator.py
@@ -7,6 +7,7 @@ from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler, DDIM
 from transformers import CLIPTextModel
 import json
 import piexif
+import copy
 torch.backends.cuda.matmul.allow_tf32 = True
 
 
@@ -58,21 +59,30 @@ class PromptGenerator:
 
 class DiffusionCreator:
     def __init__(self, modelWeightRoot='.',
-                 defaultModel='runwayml/stable-diffusion-v1-5',
-                 defaultDType=torch.bfloat16,
+                 modelDictList=[{'name':'runwayml/stable-diffusion-v1-5','factor':1}],
+                 defaultDType=torch.float16,
                  useXformers=False) -> None:
         self.modelWeightRoot = modelWeightRoot
-        self.defaultModel = defaultModel
+        self.modelDictList = modelDictList
         self.defaultDType = defaultDType
         self.useXformers = useXformers
         self.randGenerator = torch.Generator()
-        self.loadModel(self.defaultModel)
+        self.loadModel()
 
-    def loadModel(self, modelName):
-        if modelName[0] == '.':
-            modelName = os.path.join(self.modelWeightRoot, modelName[1:])
+    def loadModel(self):
+        self.loadMultiModel(self.modelDictList)
+        baseModelName = self.modelDictList[0]['name']
+        tempUNet = copy.deepcopy(self.modelUNetList[baseModelName])
+        if len(self.modelDictList)>1:
+            unetWeight = self.blendModel(self.modelDictList)
+            tempUNet.load_state_dict(unetWeight)
+        
+        if baseModelName[0] == '.':
+            baseModelName = os.path.join(self.modelWeightRoot, baseModelName[1:])
+
         self.pipe = StableDiffusionLongPromptWeightingPipeline.from_pretrained(
-            modelName, cache_dir=self.modelWeightRoot,
+            baseModelName, cache_dir=self.modelWeightRoot,
+            unet = tempUNet,
             requires_safety_checker=False,
             feature_extractor=None,
             safety_checker=None,
@@ -83,17 +93,34 @@ class DiffusionCreator:
         if self.useXformers:
             self.pipe.enable_xformers_memory_efficient_attention()
 
-    def loadBlendModel(self, blendParamDictList):
+
+
+    def loadMultiModel(self, blendParamDictList):
         self.modelUNetList = {}
         for blendParamDict in blendParamDictList:
             modelNameRaw = blendParamDict['name']
             if modelNameRaw[0] == '.':
                 modelName = os.path.join(
                     self.modelWeightRoot, modelNameRaw[1:], 'unet')
+                self.modelUNetList[modelNameRaw] = UNet2DConditionModel.from_pretrained(
+                        modelName,
+                         cache_dir=self.modelWeightRoot,
+                         torch_dtype=self.defaultDType)                
             else:
-                raise RuntimeError('Only support local model weights.')
-            self.modelUNetList[modelNameRaw] = UNet2DConditionModel.from_pretrained(
-                modelName, cache_dir=self.modelWeightRoot)
+                modelName = modelNameRaw
+                pipe = StableDiffusionPipeline.from_pretrained(
+                            modelName, cache_dir=self.modelWeightRoot,
+                            requires_safety_checker=False,
+                            feature_extractor=None,
+                            safety_checker=None,
+                            torch_dtype=self.defaultDType,
+                            text_encoder=None
+                        )    
+
+                self.modelUNetList[modelNameRaw] = pipe.unet
+                del pipe  
+                          
+
 
     def blendModel(self, blendParamDictList):
         firstModelParamDict = blendParamDictList[0]
@@ -112,7 +139,10 @@ class DiffusionCreator:
                 tempStateDict[weightKey] += self.modelUNetList[modelName].state_dict()[
                     weightKey]*modelFactor
 
-        self.pipe.unet.load_state_dict(tempStateDict)
+        return tempStateDict
+    
+    def blendInRuntime(self,blendParamDictList):
+        self.pipe.unet.load_state_dict(self.blendModel(blendParamDictList))
 
     def getExif(self, jsonDict):
         # https://stackoverflow.com/questions/52729428/how-to-write-custom-metadata-into-jpeg-with-python/63400376#63400376
@@ -139,7 +169,7 @@ class DiffusionCreator:
         genMetaInfoDict = {
             'seed': seed,
             'prompt': prompt,
-            'model': self.defaultModel
+            'model': self.modelDictList
         }
 
         argDict = {
@@ -167,25 +197,25 @@ class DiffusionCreator:
 
 
 # example
+# from Creator import DiffusionCreator,RandomArtistGenerator,RandomImageSizeGenerator,PromptGenerator
+# import torch
+
 # if __name__ == "__main__":
-#     creator = DiffusionCreator(modelWeightRoot=r'path-to-model-weight',
-#                                defaultModel='.model1',  # To use local weight you should start with "."
-#                                defaultDType=torch.bfloat16,
+#     modelDictList = ({'name': 'runwayml/stable-diffusion-v1-5', 'factor': 0.4},
+#                           {'name': 'Linaqruf/anything-v3.0', 'factor': 0.6})
+#     creator = DiffusionCreator(modelWeightRoot=r'../StableDiffusionWeight',
+#                                modelDictList=modelDictList,  # To use local weight you should start with "."
+#                                defaultDType=torch.float16,
 #                                useXformers=True)
 
-#     blendParamDictList = ({'name': '.model1', 'factor': 0.4},
-#                           {'name': '.model2', 'factor': 0.6})
-
-#     creator.loadBlendModel(blendParamDictList)
-#     creator.blendModel(blendParamDictList)
 #     creator.to('cuda')
 #     artistGen = RandomArtistGenerator()
 #     sizeGen = RandomImageSizeGenerator(sizeSet='big')
 #     promptGen = PromptGenerator(
 #         [  
-#             'fdgdgdgdfgdgf'
+#             'illustration of beautiful huge dahlia garden'
 #         ],
-#         negativePrompt='closed eyes,slanted eyes,ugly,Polydactyly,handicapped,extra fingers,fused fingers,poorly drawn hands,extra legs,one leg,woman underwear,low quality,low res,blurry,draft,text,watermark,signature,two heads,mutated hands,mutation,deformed, bad anatomy, bad proportions,too many fingers,morbid, mutilated, extra limbs,disfigured,missing arms,missing legs,extra arms,malformed limbs',
+#         negativePrompt=None,#'closed eyes,slanted eyes,ugly,Polydactyly,handicapped,extra fingers,fused fingers,poorly drawn hands,extra legs,one leg,woman underwear,low quality,low res,blurry,draft,text,watermark,signature,two heads,mutated hands,mutation,deformed, bad anatomy, bad proportions,too many fingers,morbid, mutilated, extra limbs,disfigured,missing arms,missing legs,extra arms,malformed limbs',
 #         artistGenerator=RandomArtistGenerator()
 #     )
 
